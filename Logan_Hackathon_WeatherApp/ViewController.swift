301ca14df5a69aaa2d237e2e5ff40c77b7ae676f
SeoDongWook <onec555@gmail.com>
2022-10-05 19:27:16 +0900

    Initial Commit

---
+++ Logan_Hackathon_WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Logan_Hackathon_WeatherApp
+//
+//  Created by DONGWOOK SEO on 2022/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

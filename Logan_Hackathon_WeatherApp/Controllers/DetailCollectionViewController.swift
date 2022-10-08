59a389817349324a2f32820d676aa3e6a7394b22
SeoDongWook <onec555@gmail.com>
2022-10-09 01:41:24 +0900

    Commit Oct 9

--- Logan_Hackathon_WeatherApp/Controllers/DetailCollectionViewController.swift
+++ Logan_Hackathon_WeatherApp/Controllers/DetailCollectionViewController.swift
@@ -13,31 +13,59 @@ class DetailCollectionViewController: UIViewController {
     
     var selectedCity: [Weather]?
     
-    private var dataSource = (0...3).map(Int.init(_:))
-    private var currentItem = -1
-    private var currentIndexPath: IndexPath {
-        IndexPath(item: self.currentItem, section: 0)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        detailCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         
-
+        detailCollectionView.dataSource = self
+        detailCollectionView.delegate = self
     }
+}
+
+extension DetailCollectionViewController: UICollectionViewDelegate{
+    
+    
+    
+    
+    
 }
 
 extension DetailCollectionViewController: UICollectionViewDataSource {
     
     //컬렉션뷰 몇개 만들지?
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        self.dataSource.count
+        return self.selectedCity!.count
     }
     
-    
+    // 각 콜렉션뷰 Cell에 대한 설정
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DetailViewCell", for: indexPath) as! CollectionViewCell
         
+        let cellId = String(describing: CollectionViewCell.self)
+        print("cellID: \(cellId)")
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CollectionViewCell
+        
+        cell.regionNameLabel.text = selectedCity![indexPath.item].name
+        cell.currentTemperatureLabel.text = selectedCity![indexPath.item].currentTemperature
+        cell.weatherIcon.image = selectedCity![indexPath.item].icon
+        cell.weatherDescriptionLabel.text = selectedCity![indexPath.item].description.rawValue
         
         return cell
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
